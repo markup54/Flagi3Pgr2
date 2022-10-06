7d499cccf5abe55d136e8a64914e70e96e8e49c6
markup54 <marzenakupka@tlen.pl>
2022-10-06 10:59:29 +0200

    wyglad aplikacji

---
+++ app/src/main/java/com/example/flagi3pgr2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.flagi3pgr2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void zlaOdpowiedz(View view) {
+        Toast.makeText(MainActivity.this,
+                R.string.zla_odp,
+                Toast.LENGTH_SHORT)
+                .show();
+    }
+}
